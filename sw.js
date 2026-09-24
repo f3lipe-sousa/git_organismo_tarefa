eeebf5c541ddc66544a19bc73bb940ec16c311dd
f3lipe-sousa <f3lipe.sousa@gmail.com>
2026-09-24 15:54:20 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,11 +6,11 @@ const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./css/style.css",
+  "./style.css",
   "./js/core.js",
   "./js/app.js",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
